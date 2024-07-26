8e60d4c2fd5573c7b38e967058a9605f410e62fa
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-07-26 11:18:45 +0200

    aggiunta costruttore video e variabile luminosità

--- src/riproduttoreMultimediale/Video.java
+++ src/riproduttoreMultimediale/Video.java
@@ -3,11 +3,13 @@ package riproduttoreMultimediale;
 public class Video extends OggettiMultimediali {
     private int durata;
     private int volume;
+    private int luminosita;
 
-    public Video(String titolo, int durata, int volume) {
+    public Video(String titolo, int durata, int volume, int luminosita) {
         super(titolo);
         this.durata = durata;
         this.volume = volume;
+        this.luminosita = luminosita;
 
     }
 }
