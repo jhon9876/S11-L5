1d8fe0fefb6cb03e277f965d28db1534d28d1730
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-07-26 11:06:20 +0200

    conclusione provvisoria classe Audio

--- src/riproduttoreMultimediale/Audio.java
+++ src/riproduttoreMultimediale/Audio.java
@@ -1,4 +1,25 @@
 package riproduttoreMultimediale;
 
-public class Audio {
+import interfaces.Riproducibile;
+
+public class Audio extends OggettiMultimediali implements Riproducibile {
+
+    private int durata;
+    private int volume;
+
+    public Audio(String titolo, int durata, int volume) {
+        super(titolo);
+        this.durata = durata;
+        this.volume = volume;
+    }
+
+    @Override
+    public void abbassaVolume() {
+        this.volume++;
+    }
+
+    @Override
+    public void alzaVolume() {
+        this.volume--;
+    }
 }
--- src/riproduttoreMultimediale/Main.java
+++ src/riproduttoreMultimediale/Main.java
@@ -2,6 +2,6 @@ package riproduttoreMultimediale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
